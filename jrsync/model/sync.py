4564257bfb439efaa2303ce4fa05d16588647a1e
Antonio Mariani <antonio.mariani@cmcc.it>
2024-08-02 16:51:27 +0200

    resolve paths when Jsync is craeted

--- jrsync/model/sync.py
+++ jrsync/model/sync.py
@@ -2,9 +2,11 @@ from datetime import datetime
 from pathlib import Path
 from typing import List
 
-from pydantic import BaseModel, Field
+from pydantic import BaseModel, Field, field_validator
+from pydantic_core.core_schema import ValidationInfo
 
 import jrsync.conf as settings
+from jrsync.utils import str_parser
 
 
 class Jsync(BaseModel):
@@ -13,3 +15,9 @@ class Jsync(BaseModel):
     dest_dir: Path
     file_to_sync: List[str] | str = Field(settings.ALL_DIRECTORY)
     day: str = Field("*")
+
+    @field_validator("source_dir", "dest_dir", mode="before")
+    @classmethod
+    def resolve_path(cls, v: str, info: ValidationInfo) -> Path:
+        resolved_str_path = str_parser.resolve_str(v, info.data["date_to_sync"])
+        return Path(resolved_str_path)
